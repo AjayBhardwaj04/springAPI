51476b7b962a6cdee953ba65f646e4307bf03974
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-23 18:57:46 -0700

    configretion for api

---
+++ src/main/java/com/example/api/comfig/configretion.java
@@ -0,0 +1,14 @@
+package com.example.api.comfig;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class configretion {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
