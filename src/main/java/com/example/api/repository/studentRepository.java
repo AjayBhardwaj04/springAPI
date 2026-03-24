c23b0f21b5f984690ba2697657b3c3d57213d1a5
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-23 19:01:13 -0700

    Repository file

--- src/main/java/com/example/api/repository/studentRepository.java
+++ src/main/java/com/example/api/repository/studentRepository.java
@@ -1,8 +1,10 @@
 package com.example.api.repository;
 
+import com.example.api.dto.studentDto;
 import com.example.api.entity.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface studentRepository extends JpaRepository<Students,Long > {
+
 
 }
