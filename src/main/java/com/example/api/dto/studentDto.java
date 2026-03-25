906656ce729badeaeb3cb26f5620b3b2a533a7a0
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-24 19:01:11 -0700

    student dto code update

--- src/main/java/com/example/api/dto/studentDto.java
+++ src/main/java/com/example/api/dto/studentDto.java
@@ -2,9 +2,11 @@ package com.example.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class studentDto {
     private long id;
     private String Name;
