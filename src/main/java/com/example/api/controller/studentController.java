74e985123f9f9d7a26d10c766f26089f8295e798
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-25 19:13:12 -0700

    code update

--- src/main/java/com/example/api/controller/studentController.java
+++ src/main/java/com/example/api/controller/studentController.java
@@ -3,8 +3,11 @@ package com.example.api.controller;
 import com.example.api.dto.studentDto;
 import com.example.api.service.studentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,12 +21,13 @@ public class studentController {
 
 
     @GetMapping("/students")
-    public List<studentDto> getStudent() {
-        return studentService.getAllStudents();
+    public  ResponseEntity< studentDto> getStudent() {
+//        return ResponseEntity.status(HttpStatus.OK).body((studentDto) studentService.getAllStudents());
+        return ResponseEntity.ok((studentDto) studentService.getAllStudents());
 
     }
     @GetMapping("/students/{ID}")
-    public studentDto getAllStudentsById( @PathVariable long ID) {
-        return studentService.getById(ID);
+    public ResponseEntity<studentDto> getAllStudentsById(@PathVariable long ID) {
+        return ResponseEntity.status(HttpStatus.OK ).body(studentService.getById(ID));
     }
 }
