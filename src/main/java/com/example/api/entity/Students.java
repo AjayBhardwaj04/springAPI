cd2e97bf109387e4bb0cbf9d95ce30588b69c532
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-28 19:31:59 -0700

    code update

--- src/main/java/com/example/api/entity/Students.java
+++ src/main/java/com/example/api/entity/Students.java
@@ -1,22 +1,40 @@
 package com.example.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import tools.jackson.databind.ObjectMapper;
 
 @Entity
 @Getter
 @Setter
+@Data
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Students {
 
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private long id;
-        private String name;
-        private String email;
+        private String id ;
+
+        @JsonSetter(nulls = Nulls.SKIP)
+        private String email = getEmail();
+
+        @JsonSetter(nulls = Nulls.SKIP)
+        private String name = getName();
+
 
 }
+
+// Usage
+
+
