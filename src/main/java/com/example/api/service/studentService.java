ec6e0774f551d58a1a35699a8e6107870f0e2b07
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-28 19:33:18 -0700

    code update

--- src/main/java/com/example/api/service/studentService.java
+++ src/main/java/com/example/api/service/studentService.java
@@ -1,11 +1,16 @@
 package com.example.api.service;
 
+import com.example.api.dto.addStudentReqDTO;
 import com.example.api.dto.studentDto;
 
 import java.util.List;
 
 public  interface studentService {
+    List<studentDto> getAllStudents(String name);
+
     List<studentDto> getAllStudents();
 
-     studentDto getById(long id);
+    studentDto getById(long id);
+
+    studentDto addNewStudent(addStudentReqDTO addStudentReqDTO);
 }
