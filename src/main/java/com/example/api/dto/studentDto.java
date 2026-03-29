fe4884856642bdd24a43bb25cc827f35a67658ab
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-28 19:31:33 -0700

    code update

--- src/main/java/com/example/api/dto/studentDto.java
+++ src/main/java/com/example/api/dto/studentDto.java
@@ -1,14 +1,24 @@
 package com.example.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.mapping.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class studentDto {
-    private long id;
-    private String name;
-    private String email;
+    private String id ;
+
+    @JsonSetter(nulls = Nulls.SKIP)
+    private String email = getEmail();
+
+    @JsonSetter(nulls = Nulls.SKIP)
+    private String name = getName();
+
 }
