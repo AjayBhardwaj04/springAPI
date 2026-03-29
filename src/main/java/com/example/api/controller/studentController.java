e4a42a9dcfec1f07045d9e1f6fe1a3177a99e61a
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-28 19:30:50 -0700

    code update

--- src/main/java/com/example/api/controller/studentController.java
+++ src/main/java/com/example/api/controller/studentController.java
@@ -1,33 +1,44 @@
 package com.example.api.controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.api.dto.addStudentReqDTO;
 import com.example.api.dto.studentDto;
 import com.example.api.service.studentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/students")
 public class studentController {
 
    private final studentService studentService;
 
 
 
-    @GetMapping("/students")
-    public ResponseEntity<List<studentDto>> getAllStudents() {
+    @GetMapping("")
+    public  ResponseEntity< List<studentDto>> getAllStudents() {
+//        return studentService.getAllStudents();
 //        return ResponseEntity.status(HttpStatus.OK).body((studentDto) studentService.getAllStudents());
-        return ResponseEntity.status(HttpStatus.OK).body( studentService.getAllStudents());
-
+//        return ResponseEntity.status(HttpStatus.OK).body( studentService.getAllStudents().stream().toList());
+          return ResponseEntity.ok(studentService.getAllStudents());
     }
-    @GetMapping("/students/{ID}")
+    @GetMapping("/{ID}")
     public ResponseEntity<studentDto> getAllStudentsById(@PathVariable long ID) {
-        return ResponseEntity.status(HttpStatus.OK ).body(studentService.getById(ID));
+//        return ResponseEntity.status(HttpStatus.OK ).body(studentService.getById(ID));
+        return ResponseEntity.ok(studentService.getById(ID));
     }
+
+    @PostMapping
+    public ResponseEntity<studentDto> addToNewStudent(@RequestBody addStudentReqDTO addStudentReqDTO) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(studentService.addNewStudent(addStudentReqDTO));
+    }
+
+
 }
